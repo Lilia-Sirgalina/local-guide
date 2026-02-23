0a7d665621568e9f50862b4a72d820e31026166a
Lilia-Sirgalina <lilia_developer@hotmail.com>
2026-02-23 16:00:21 +0100

    Making app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router";
+import Home from './Home';
+import Beaches from './Beaches';
+import Restaurants from './Restaurants';
+import Barcelona from './Barcelona';
 
 function App() {
   
 
   return (
     <>
-      <h1>Hello</h1>
+      <h1>LOCALE GUIDE</h1>
+
+      <div>
+        <Router>
+      <nav>
+        <Link to="/" className="link">Home</Link>
+        <Link to="/beaches" className="link">Beaches</Link>
+        <Link to="/restaurants" className="link">Restaurants</Link>
+        <Link to="/Barcelona" className="link">Barcelona/Girona</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/beaches" element={<Beaches />} />
+        <Route path="/restaurants" element={<Restaurants />} />
+        <Route path="/Barcelona" element={<Barcelona />} />
+      </Routes>
+    </Router>    
+      </div>
     </>
   )
 }
---
+++ src/Beaches.jsx
@@ -0,0 +1,9 @@
+const Beaches = () => {
+    return (
+        <div>
+            <h1>Beaches component</h1>
+        </div>
+    )
+}
+
+export default Beaches;
